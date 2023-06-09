61648dda6021062a7d489f36ceef1ba9b47ad9dd
AshDesignsllc <support@ashdesigns.co>
2023-06-09 14:10:01 -0700

    Add event listener to button

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ console.log(button);
 var cat = document.querySelector(".cat");
 console.log(cat);
 
-button.addEventListener('click', function() {
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!"
+    button.classList.add("disappear");
+  } else {
     cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+  }
 });
